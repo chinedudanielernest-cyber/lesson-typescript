af2eca1a54f4921f3ca9b8499f82987e0cb90e1e
Daniel Ernest <chinedu.daniel.ernest@gmail.com>
2025-11-12 18:26:38 +0100

    Created a typescript compiler and configured it

---
+++ app.ts
@@ -0,0 +1,17 @@
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+  Tel: number,
+  age: number 
+}
+
+const user: IUser = {
+    id: 1,
+    name: "John Doe",
+    email: "pin4eva@gmail.com",
+    Tel: 1234567890,
+    age: 30
+}
+
+console.log(user);
